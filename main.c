8781aafc0cbbc9b15722fb282aab23757d268d26
fraczem1 <dmitriwear@gmail.com>
2023-08-04 22:22:29 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "xor.h"
+
+
+int main(int argc, char **argv) 
+{
+	int opt;
+
+	char *file_path= NULL;
+	char *key = NULL;
+
+	while ((opt = getopt(argc, argv, "f:k:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'f':
+				file_path = optarg;
+				break;
+			case 'k':
+				key = optarg;
+				break;
+			case '?':
+				if (optopt == 'f' || optopt == 'k')
+				{
+                    			fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+				}
+				else
+				{
+			    		fprintf(stderr, "Unknown option %c.\n", optopt);
+				}
+				return 1;
+			default:
+				fprintf(stderr, "Invalid option.\n");
+				return 1;
+		}
+	}
+	
+	if (file_path == NULL || key == NULL)
+	{
+		fprintf(stderr, "Empty argument.\n");
+		return 2;
+	}
+
+	printf("path: %s\n", file_path);
+	printf("key: %s\n", key);
+
+	switch (xor(file_path, key)) 
+	{
+		case 1:
+			fprintf(stderr, "Couldn't read %s file.\n", file_path);
+			return 3;
+		case 2:
+			fprintf(stderr, "Couldn't write to %s file.\n", file_path);
+			return 4;
+	}
+
+	return 0;
+}
